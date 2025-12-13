03c916c9da89b138751e18ab98a0c05ac2c4ff83
Punyanator <bellodanny99@gmail.com>
2025-12-13 10:50:45 +0100

    final edits

--- app.py
+++ app.py
@@ -2,15 +2,20 @@ from flask import Flask, render_template, request
 import os
 import re
 from google import genai
+import time
+from google.genai.errors import ClientError
 
 app = Flask(__name__)
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=GEMINI_API_KEY)
 
 
+
+
+
 def preprocess(text):
     text = text.lower().strip()
-    text = re.sub(r"[^a-z0-9\\s]", "", text)
+    text = re.sub(r"[^a-z0-9\s]", "", text)
     return text
 
 def call_gemini(prompt, model="gemini-2.5-flash"):
